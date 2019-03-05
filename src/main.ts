649427724b27a716d6b4aaa7b22a626c1497efc6
Ford Hurley <ford.hurley@gmail.com>
2019-03-05 12:59:25 -0500

    Initialize shader with source

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,26 @@ import {Webview} from "./webview";
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand("glslPreview.show", () => {
         Webview.createOrShow(context.extensionPath);
+        updateShader();
     }));
 
-    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(onDidChangeTextDocument));
+    context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(updateShader));
 }
 
-function onDidChangeTextDocument(e: vscode.TextDocumentChangeEvent) {
-    console.log("changed:", e.document.uri.toString());
-    if (Webview.current) {
-        Webview.current.updateShader(e.document.getText());
+function updateShader() {
+    const source = getShaderSource();
+    if (source && Webview.current) {
+        Webview.current.updateShader(source);
     }
+}
+
+function getShaderSource(): string | undefined {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+    if (editor.document.languageId !== "glsl") {
+        return;
+    }
+    return editor.document.getText();
 }
