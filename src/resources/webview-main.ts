8fcb32ecb231f706630fb55dc740fd0cbfd1d560
Ford Hurley <ford.hurley@gmail.com>
2019-03-05 10:28:50 -0500

    Show a ShaderCanvas in the webview

--- src/resources/webview-main.ts
+++ src/resources/webview-main.ts
@@ -1,5 +1,19 @@
-import { greet } from "./test-submodule";
+import {ShaderCanvas} from "shader-canvas";
 
-const h1 = document.createElement("h1");
-h1.textContent = greet();
-document.body.appendChild(h1);
+const size = Math.min(window.innerWidth, window.innerHeight);
+
+const shader = new ShaderCanvas();
+shader.setShader(`
+    precision mediump float;
+    uniform vec2 u_resolution;
+    void main() {
+        vec2 uv = gl_FragCoord.xy / u_resolution;
+        gl_FragColor = vec4(uv.x, 0.0, uv.y, 1.0);
+    }
+`);
+
+shader.setSize(size, size);
+shader.setUniform("u_resolution", shader.getResolution());
+shader.render();
+
+document.body.appendChild(shader.domElement);
