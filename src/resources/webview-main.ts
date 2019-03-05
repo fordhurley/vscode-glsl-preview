833c9c0407bffb0289e378bb0378456a42a10efb
Ford Hurley <ford.hurley@gmail.com>
2019-03-05 12:53:19 -0500

    Add a Shader class in the webview

--- src/resources/webview-main.ts
+++ src/resources/webview-main.ts
@@ -1,45 +1,59 @@
 import {ShaderCanvas} from "shader-canvas";
 
-let source = `
-    precision mediump float;
-    void main() {
-        gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-    }
-`;
-
-const shader = new ShaderCanvas();
-shader.setShader(source);
-document.body.appendChild(shader.domElement);
-
-window.addEventListener("message", (event) => {
-    if (event.data.command === "updateShader") {
-        source = event.data.source;
-        shader.setShader(source);
-        resize();
-        shader.render();
-    }
-});
-
-function testUniform(type: string, name: string) {
+function testUniform(type: string, name: string, source: string) {
     const re = new RegExp(`^\\s*uniform\\s+${type}\\s+${name}`, "m");
     return re.test(source);
 }
 
-function resize() {
-    const size = Math.min(window.innerWidth, window.innerHeight);
-    shader.setSize(size, size);
-    if (testUniform("vec2", "u_resolution")) {
-        shader.setUniform("u_resolution", shader.getResolution());
-    }
-}
-resize();
-window.addEventListener("resize", resize);
+class Shader {
+    private source: string = `
+        precision mediump float;
+        void main() {
+            gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+        }
+    `;
 
-function animate(timestamp: number) {
-    window.requestAnimationFrame(animate);
-    if (testUniform("float", "u_time")) {
-        shader.setUniform("u_time", timestamp / 1000);
+    public shaderCanvas = new ShaderCanvas();
+
+    public constructor() {
+        this.setSource(this.source);
+
+        this.resize = this.resize.bind(this);
+
+        this.resize();
+        window.addEventListener("resize", this.resize);
+
+        this.animate = this.animate.bind(this);
+        window.requestAnimationFrame(this.animate);
     }
-    shader.render();
+
+    public setSource(source: string) {
+        this.source = source;
+        this.shaderCanvas.setShader(this.source);
+    }
+
+    public resize() {
+        const size = Math.min(window.innerWidth, window.innerHeight);
+        this.shaderCanvas.setSize(size, size);
+        if (testUniform("vec2", "u_resolution", this.source)) {
+            this.shaderCanvas.setUniform("u_resolution", this.shaderCanvas.getResolution());
+        }
+    }
+
+    public animate(timestamp: number) {
+        window.requestAnimationFrame(this.animate);
+        if (testUniform("float", "u_time", this.source)) {
+            this.shaderCanvas.setUniform("u_time", timestamp / 1000);
+        }
+        this.shaderCanvas.render();
+    }
 }
-window.requestAnimationFrame(animate);
+
+const shader = new Shader();
+document.body.appendChild(shader.shaderCanvas.domElement);
+
+window.addEventListener("message", (event) => {
+    if (event.data.command === "updateShader") {
+        shader.setSource(event.data.source);
+    }
+});
